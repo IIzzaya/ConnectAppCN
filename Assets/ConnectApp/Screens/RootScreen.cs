ffbf64b21909e7921d505ac2841a6dc878d3d5e3
wangshuang <wangshuang@unity3d.com>
2019-07-30 11:40:12 +0800

    update: add jump button.

--- Assets/ConnectApp/Screens/RootScreen.cs
+++ Assets/ConnectApp/Screens/RootScreen.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using ConnectApp.Components;
 using ConnectApp.Constants;
+using ConnectApp.Main;
 using Unity.UIWidgets.painting;
 using Unity.UIWidgets.rendering;
+using Unity.UIWidgets.ui;
 using Unity.UIWidgets.widgets;
 using UnityEngine;
 using Color = Unity.UIWidgets.ui.Color;
+using Image = Unity.UIWidgets.widgets.Image;
 
 namespace ConnectApp.screens {
     public class RootScreen : StatefulWidget {
@@ -15,6 +18,15 @@ namespace ConnectApp.screens {
     }
 
     class _RootScreen : State<RootScreen> {
+
+        bool jumpToMain;
+        public override void initState() {
+            base.initState();
+            this.jumpToMain = false;
+        }
+        
+        
+
         public override Widget build(BuildContext context) {
             if (Application.platform == RuntimePlatform.Android) {
                 return new Container(
@@ -28,6 +40,19 @@ namespace ConnectApp.screens {
                         mainAxisAlignment: MainAxisAlignment.end,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: new List<Widget> {
+                            new CustomButton(
+                                child: new Text(
+                                    "跳到主页",
+                                    style: new TextStyle(
+                                        height: 1.33f,
+                                        fontSize: 30,
+                                        fontFamily: "Roboto-Regular",
+                                        color: CColors.Blue
+                                    ),
+                                    textAlign: TextAlign.center
+                                ),
+                                onPressed: () => { Router.navigator.pushNamed(MainNavigatorRoutes.Main); }
+                            ),
                             new Container(
                                 width: 228,
                                 height: 40,
